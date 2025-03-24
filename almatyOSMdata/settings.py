f00bf1e4b5a15f00ef82b95b250fd3b60f5ecfb0
Dmitrii Vasilev <wasilev1994@gmail.com>
2025-03-24 15:56:37 +0500

    move sensitive credentials to .env

--- almatyOSMdata/settings.py
+++ almatyOSMdata/settings.py
@@ -15,6 +15,8 @@ import os
 from dotenv import load_dotenv
 from django.contrib.messages import constants as messages
 
+load_dotenv() # Загрузка переменных окружения из .env
+
 # Для вывода сообщений о некорректно введённых данных
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
@@ -34,12 +36,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*@0jpa15jkmcjf-ym3hh+va7xmlb6n9gqx$yq(byrf1e2im&n@'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
@@ -91,8 +93,6 @@ WSGI_APPLICATION = 'almatyOSMdata.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-load_dotenv()
 
 DATABASES = {
     'default': {
