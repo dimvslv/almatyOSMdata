60b72fbd23c15a530bb65d7c04878be213a1aaef
Dmitrii Vasilev <wasilev1994@gmail.com>
2025-03-24 15:25:42 +0500

    add bus stops and roads (filtered by highway type) to the map

--- apps/osm_data/urls.py
+++ apps/osm_data/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import lines_geojson
+from .views import points_geojson
 
 urlpatterns = [
     path('lines/', lines_geojson, name='lines_geojson'),
+    path('points/', points_geojson, name='points_geojson'),
 ]
--- apps/osm_data/views.py
+++ apps/osm_data/views.py
@@ -2,9 +2,20 @@ import json
 from django.http import JsonResponse
 from django.core.serializers import serialize
 from .models import Lines
+from .models import Points
 
 def lines_geojson(request):
-    lines = Lines.objects.using('osm_db').all()[:10000]
+    lines = Lines.objects.using('osm_db').filter(highway__in=['primary', 'secondary'])
     geojson_str = serialize('geojson', lines, geometry_field='geom', fields=('osm_id', 'name', 'highway'))
     geojson_data = json.loads(geojson_str)  # Преобразуем строку в объект
+    
     return JsonResponse(geojson_data, safe=False)
+
+def points_geojson(request):
+    points = Points.objects.using('osm_db').filter(highway='bus_stop')[:500]
+    geojson_str = serialize('geojson', points, geometry_field='geom', fields=('osm_id', 'name', 'highway'))
+    geojson_data = json.loads(geojson_str)
+
+    return JsonResponse(geojson_data, safe=False)
+
+
